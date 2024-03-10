57a3d05011614df2e0e84127769678f217b0679e
Daniil Zaytsev <kupernewfag@gmail.com>
2024-03-10 19:26:33 +0300

    feat(code-fix): implement code fix provider for IM0002
    Refs: 3

--- ImmutableAnalyzer/ImmutableAnalyzer/PropertyAnalyzers/SetAccessor/SetAccessorCodeFixProvider.cs
+++ ImmutableAnalyzer/ImmutableAnalyzer/PropertyAnalyzers/SetAccessor/SetAccessorCodeFixProvider.cs
@@ -9,69 +9,80 @@ using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Editing;
+using AccessorModifier = System.Func<
+    Microsoft.CodeAnalysis.CSharp.Syntax.AccessorDeclarationSyntax,
+    Microsoft.CodeAnalysis.CSharp.Syntax.AccessorDeclarationSyntax
+>;
 
 namespace ImmutableAnalyzer.PropertyAnalyzers.SetAccessor;
 
-[ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(SetAccessorCodeFixProvider))]
-[Shared]
+/// <summary>
+/// Provides code fix for <see cref="SetAccessorAnalyzer"/> diagnostic.
+/// </summary>
+[ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(SetAccessorCodeFixProvider)), Shared]
 public class SetAccessorCodeFixProvider : CodeFixProvider
 {
-    public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(SetAccessorAnalyzer.DiagnosticId);
-    public override FixAllProvider? GetFixAllProvider() => null;
+    /// <summary>
+    /// Title template to this code fix provider.
+    /// </summary>
+    private const string Title = "Change property accessor to '{0}'";
+
+    /// <inheritdoc />
+    public override ImmutableArray<string> FixableDiagnosticIds =>
+        ImmutableArray.Create(SetAccessorAnalyzer.DiagnosticId);
+
+    /// <inheritdoc />
+    public override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+
+    /// <inheritdoc />
     public override async Task RegisterCodeFixesAsync(CodeFixContext context)
     {
         var diagnostic = context.Diagnostics.Single();
-        var diagnosticSpan = diagnostic.Location.SourceSpan;
 
         var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
-
-        var diagnosticNode = root?.FindNode(diagnosticSpan);
-
-        if (diagnosticNode is not PropertyDeclarationSyntax declaration)
+        if (root?.FindNode(diagnostic.Location.SourceSpan) is not AccessorDeclarationSyntax diagnosticNode)
             return;
 
         context.RegisterCodeFix(
             CodeAction.Create(
-                title: "Change property accessor to 'private set'",
+                title: string.Format(Title, "private set"),
                 createChangedDocument: ct => ChangePropertyAccessorAsync(
-                    context.Document,
-                    declaration,
-                    SyntaxKind.PrivateKeyword,
-                    ct),
+                    context.Document, diagnosticNode,
+                    accessor => accessor.WithModifiers(
+                        new SyntaxTokenList(SyntaxFactory.Token(SyntaxKind.PrivateKeyword))
+                    ), ct),
                 equivalenceKey: $"{SetAccessorAnalyzer.DiagnosticId}Fix"),
             diagnostic
         );
 
         context.RegisterCodeFix(
             CodeAction.Create(
-                title: "Change property accessor to 'init'",
+                title: string.Format(Title, "init"),
                 createChangedDocument: ct => ChangePropertyAccessorAsync(
-                    context.Document,
-                    declaration,
-                    SyntaxKind.InitKeyword,
-                    ct),
+                    context.Document, diagnosticNode,
+                    accessor => accessor.WithKeyword(SyntaxFactory.Token(SyntaxKind.InitKeyword)), ct),
                 equivalenceKey: $"{SetAccessorAnalyzer.DiagnosticId}Fix"),
             diagnostic
         );
     }
 
+    /// <summary>
+    /// Changes access modifier with given lambda.
+    /// </summary>
+    /// <param name="document">Document to change.</param>
+    /// <param name="accessor">Accessor to change.</param>
+    /// <param name="modifier">Func to get modified accessor syntax node.</param>
+    /// <param name="ct">Token to cancel task.</param>
+    /// <returns></returns>
     private static async Task<Document> ChangePropertyAccessorAsync(
         Document document,
-        BasePropertyDeclarationSyntax property,
-        SyntaxKind keyword,
+        AccessorDeclarationSyntax accessor,
+        AccessorModifier modifier,
         CancellationToken ct = default
     )
     {
         var editor = await DocumentEditor.CreateAsync(document, ct);
-
-        var setter = property.AccessorList?.Accessors
-            .FirstOrDefault(a => a.IsKind(SyntaxKind.SetAccessorDeclaration));
-        if (setter == null)
-            return document;
-
-        var newSetter = setter.WithModifiers(new SyntaxTokenList(SyntaxFactory.Token(keyword)));
-
-        editor.ReplaceNode(setter, newSetter);
+        editor.ReplaceNode(accessor, modifier.Invoke(accessor));
         return editor.GetChangedDocument();
     }
 }
